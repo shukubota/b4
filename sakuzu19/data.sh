cce6062a91cc0b20804bbc195b6d3d8a8d6d7726
shukubota <kubotashu0421@gmail.com>
2017-06-21 09:48:24 +0900

    Add files via upload

---
+++ sakuzu19/data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp kubota@10.226.168.181:/box03/TRMM/V7/L2/2A25/*/*/*HDF.gz /media/kubotashu/ボリューム/data/
+
+scp kubota@10.226.168.181:/box03/TRMM/V7/L2/2A25/*/*/*HDF.Z /media/kubotashu/ボリューム/data/
